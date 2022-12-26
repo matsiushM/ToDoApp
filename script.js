b7e795cc5139af13f4d62e8ad2732d18b992d7bd
Максим <matiush2018@yandex.ru>
2022-12-26 19:57:03 +0300

    fixed js

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
 
-let buttonSave = document.getElementById("saveButton");
+const buttonSave = document.getElementById("saveButton");
 const finishedButton = document.getElementById("finished");
-const tabTask = document.getElementById("tabTask");
+const deleteButton = document.querySelector("#delete");
+const tabTask = document.querySelector("#tabTask");
 let point = 1;
 
 buttonSave.addEventListener("click", saveTask);
-finishedButton.addEventListener("click", finishedTask)
+finishedButton.addEventListener("click", finishedTask);
+tabTask.addEventListener("click", deleteTask);
 
 function saveTask() {
-
-    let task = document.getElementById("task").value;
+    const task = document.getElementById("task").value;
 
     const taskHTMl = `<div class="container-tab__task">
             <div class="container-tab__task__number">${++point}</div>
             <div class="container-tab__task__txt">${task}</div>
-            <button class="container-tab__task__button" id = "finished">FINISHED</button>
+
+            <div class="container-tab__task__buttons">
+                <button type="button" class="container-tab__task__button__delete" id="delete">DELETE</button>
+                <button type="button" class="container-tab__task__button__finished" id="finished">FINISHED</button>
+            </div>
         </div>`;
     tabTask.insertAdjacentHTML("beforeend", taskHTMl);
+
 }
 
-function finishedTask(event) {
 
+function deleteTask(event) {
     console.log(event.target);
+    if(event.target.id === "delete"){
 
+        const node =  event.target.closest(".container-tab__task");
+        console.log(node);
+        node.remove();
+        --point;
+    }
+}
+function finishedTask(event) {
     if(event.target.id === "finished"){
-      const node =  event.target.closest(".container-tab__task");
-      console.log(node);
-      node.remove();
-      --point;
+        console.log("Done");
     }
 }
